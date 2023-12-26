cc4d3955c1551effef2126bff8c88902e1281ea2
Hansen Harly <hansenharly123@gmail.com>
2023-12-26 16:07:18 -0500

    move prefabs to resources folder, added spawner script

---
+++ HellScape/Assets/Script/Spawner.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform spawnPoint;
+
+    private List<GameObject> enemyPrefabs = new List<GameObject>();
+    private List<Transform> spawnPoints = new List<Transform>();
+
+    private GameObject boss;
+
+    private int waveNum;
+    private int enemyToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waveNum = 1;
+
+        enemyPrefabs.AddRange(Resources.LoadAll<GameObject>("Prefab/Enemy"));
+        foreach(GameObject enemy in enemyPrefabs)
+        {
+            if (enemy.name == "Orc")
+            {
+                boss = enemy;
+                break;
+            }
+        }
+
+        foreach (Transform child in spawnPoint.GetComponentInChildren<Transform>()) 
+        {
+            spawnPoints.Add(child);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
