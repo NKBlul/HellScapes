1a48762ca86e2f8f865d03c9b3d2ac8603cd0bff
Hansen Harly <hansenharly123@gmail.com>
2023-12-28 15:18:52 -0500

    added settings credits and quit functionality
    added scene manager

---
+++ HellScape/Assets/Script/Manager/ScenesManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+    public static ScenesManager instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void LoadScene(string scene)
+    {
+        //PlayButtonPressedAudio();
+        SceneManager.LoadScene(scene);
+    }
+
+    public void PlayGame()
+    {
+        LoadScene("GameScene");
+    }
+
+    public void MainMenu()
+    {
+        LoadScene("MenuScene");
+    }
+
+    public void QuitGame()
+    {
+        //PlayButtonPressedAudio();
+        Application.Quit();
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "MenuScene")
+        {
+
+        }
+        else if (scene.name == "GameScene")
+        {
+
+        }
+    }
+}
