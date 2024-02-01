f2379331dcd508457d06a9398aae236e578df1bd
Hansen Harly <hansenharly123@gmail.com>
2024-02-01 16:29:23 -0500

    added master audio with audio mixer

--- HellScape/Assets/Script/Manager/SettingsManager.cs
+++ HellScape/Assets/Script/Manager/SettingsManager.cs
@@ -1,54 +1,69 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class SettingsManager : MonoBehaviour
 {
-    public Slider musicSlider, sfxSlider;
-    public TextMeshProUGUI musicVolume, sfxVolume;
+    public AudioMixer audioMixer;
+    public Slider masterSlider, musicSlider, sfxSlider;
+    public TextMeshProUGUI masterVolume, musicVolume, sfxVolume;
+    private int logVolume = 20;
 
     private void Start()
     {
-        if (PlayerPrefs.HasKey("musicVolume"))
+        if (PlayerPrefs.HasKey("masterVolume") || PlayerPrefs.HasKey("musicVolume") || PlayerPrefs.HasKey("sfxVolume"))
         {
-            LoadMusicVolume();
+            LoaddAllVolume();
         }
-        if (PlayerPrefs.HasKey("sfxVolume"))
+        else
         {
-            LoadSFXVolume();
+            SetAllVolume();
         }
     }
 
-    public void MusicVolume()
-    {
-        float convertedMusicVolume = RoundToNearestDecimalPoint(musicSlider.value);
-        AudioManager.instance.MusicVolume(convertedMusicVolume);
-        musicVolume.text = convertedMusicVolume.ToString();
-        PlayerPrefs.SetFloat("musicVolume", convertedMusicVolume);
+    public void SetMasterVolume()
+    {       
+        float volume = (float)Math.Round(masterSlider.value, 2);
+        audioMixer.SetFloat("master", Mathf.Log10(volume) * logVolume);
+        masterVolume.text = (volume * 100).ToString();
+        PlayerPrefs.SetFloat("masterVolume", volume);
     }
 
-    public void SfxVolume() 
+    public void SetMusicVolume()
     {
-        float convertedSFXVolume = RoundToNearestDecimalPoint(sfxSlider.value);
-        AudioManager.instance.SFXVolume(convertedSFXVolume);
-        sfxVolume.text = convertedSFXVolume.ToString();
-        PlayerPrefs.SetFloat("sfxVolume", convertedSFXVolume);
+        float volume = (float)Math.Round(musicSlider.value, 2);
+        audioMixer.SetFloat("music", Mathf.Log10(volume) * logVolume);
+        musicVolume.text = (volume * 100).ToString();
+        PlayerPrefs.SetFloat("musicVolume", volume);
     }
 
-    private void LoadMusicVolume()
+    public void SetSfxVolume() 
     {
+        float volume = (float)Math.Round(sfxSlider.value, 2);
+        audioMixer.SetFloat("sfx", Mathf.Log10(volume) * logVolume);
+        sfxVolume.text = (volume * 100).ToString();
+        PlayerPrefs.SetFloat("sfxVolume", volume);
+    }
+
+    private void SetAllVolume()
+    {
+        SetMasterVolume();
+        SetMusicVolume();
+        SetSfxVolume();
+    }
+
+    private void LoaddAllVolume()
+    {
+        masterSlider.value = PlayerPrefs.GetFloat("masterVolume");
         musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
-    }
-
-    private void LoadSFXVolume()
-    {
         sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume");
-    }
 
-    private float RoundToNearestDecimalPoint(float value)
-    {
-        return Mathf.Round(value * 10.0f) / 10.0f;
+        SetAllVolume();
     }
 }
