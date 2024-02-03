e0e665296afea4c033332360ec3f469307572bc4
Hansen Harly <hansenharly123@gmail.com>
2024-02-02 22:38:49 -0500

    added hover to see if mouse on button

--- HellScape/Assets/Script/HoverButtons.cs
+++ HellScape/Assets/Script/HoverButtons.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class HoverButtons : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
+public class HoverButtons : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
+    Color32 defaultColor = new Color32(48, 48, 48, 0);
+    Color32 hoverColor = new Color32(48, 48, 48, 48);
+    private void Start()
+    {
+        GetComponent<Image>().color = defaultColor;
+        Debug.Log(defaultColor);
+    }
+
     public void OnPointerEnter(PointerEventData eventData)
     {
         AudioManager.instance.PlaySFX("ButtonHover");
+        GetComponent<Image>().color = hoverColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        GetComponent<Image>().color = defaultColor;
     }
 
     public void OnPointerClick(PointerEventData eventData)
